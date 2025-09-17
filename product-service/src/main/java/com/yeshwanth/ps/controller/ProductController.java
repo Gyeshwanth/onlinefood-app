c3010a017f1865483710956d8dcc22534e2d7a12
Gyeshwanth <82267678+Gyeshwanth@users.noreply.github.com>
2025-09-17 17:24:39 +0530

    Update ProductController.java

--- product-service/src/main/java/com/yeshwanth/ps/controller/ProductController.java
+++ product-service/src/main/java/com/yeshwanth/ps/controller/ProductController.java
@@ -1,9 +1,10 @@
 package com.yeshwanth.ps.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.yeshwanth.ps.dto.ProductRequest;
 import com.yeshwanth.ps.dto.ProductResponse;
 import com.yeshwanth.ps.service.ProductService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -15,12 +16,24 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/api/products")
 @RequiredArgsConstructor
 public class ProductController {
-
+    
     private final ProductService productService;
-
+    
     @PostMapping(value = "/add", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<?> addProduct(@Valid @RequestPart("product") ProductRequest product, @RequestPart  MultipartFile file) {
-        ProductResponse productResponse = productService.addProduct(product, file);
+    public ResponseEntity<?> addProduct(
+            @RequestPart("product") String product, 
+            @RequestPart MultipartFile file )
+    {
+        ObjectMapper om = new ObjectMapper();
+        ProductRequest req = null;
+        
+        try {
+            req = om.readValue(product, ProductRequest.class);
+        } catch (JsonProcessingException ex) {
+            return new ResponseEntity<>("Invalid JSON format", HttpStatus.BAD_REQUEST);
+        }
+        
+        ProductResponse productResponse = productService.addProduct(req, file);
         return new ResponseEntity<>(productResponse, HttpStatus.CREATED);
     }
 }
