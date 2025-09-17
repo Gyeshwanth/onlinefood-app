a4d428dd99cbeb8f0e93a4dbc83d01d5abdaafdc
yeshwanth gosi <yeshu96183@gmail.com>
2025-09-17 14:56:07 +0530

    missing import for conditional propertry

--- product-service/src/main/java/com/yeshwanth/ps/config/AwsConfig.java
+++ product-service/src/main/java/com/yeshwanth/ps/config/AwsConfig.java
@@ -7,6 +7,7 @@ import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 
 @Configuration
 public class AwsConfig {
